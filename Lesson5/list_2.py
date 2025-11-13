8be7648e70b322f9bb9622de0f25aeaf66a02d9d
CeRBeR <deskoor@mail.ru>
2025-11-13 21:02:48 +0300

    Version4

---
+++ Lesson5/list_2.py
@@ -0,0 +1,21 @@
+'''
+Запросить по очереди у пользователя 5 имен. Добавить все в список. 
+Отсортировать. 
+Вывести на экран.
+Вывести True при наличии в списке имени 'Вася'
+'''
+
+zap = "Ведите имя: "
+
+n1 = input(zap)
+n2 = input(zap)
+n3 = input(zap)
+n4 = input(zap)
+n5 = input(zap)
+
+spis_name = [n1, n2, n3, n4, n5]
+
+spis_name.sort()
+print(spis_name)
+
+print('Имя Вася:', 'Вася' in spis_name)
